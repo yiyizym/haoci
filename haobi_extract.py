66444d943955967aaab1f77e5cdefc435a328eba
jude <jude.zhu@whalemove.com>
2017-03-30 16:02:10 +0800

    clean

--- haobi_extract.py
+++
@@ -1,7 +0,0 @@
-import re
-other_pattern = re.compile(r'|'.join(['[^不]像','似','一样','如','仿','成了','俨然']))
-with open('weicheng_biyu_only.txt', 'r') as inputfile:
-  with open('haobi_extracted.txt', 'w') as outputfile:
-    for line in inputfile:
-      if re.search('好比', line) != None and re.search(other_pattern, line) == None:
-        outputfile.write(line)
--- yiyang_extract.py
+++
@@ -1,7 +0,0 @@
-import re
-other_pattern = re.compile(r'|'.join(['[^不]像','似','如','仿','好比','成了','俨然']))
-with open('weicheng_biyu_only.txt', 'r') as inputfile:
-  with open('yiyang_extracted.txt', 'w') as outputfile:
-    for line in inputfile:
-      if re.search('一样', line) != None and re.search(other_pattern, line) == None:
-        outputfile.write(line)
